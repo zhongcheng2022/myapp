fd01f874d1a3e4dbc19323022eb5e04ddf5ef50d
Cheng Zhong <cheng.zhong@accenture.com>
2022-07-24 14:08:11 +0000

    commit 1

---
+++ srv/server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const cfenv = require('cfenv');
+const appEnv = cfenv.getAppEnv();
+const xsenv = require('@sap/xsenv');
+xsenv.loadEnv();
+const services = xsenv.getServices({
+    uaa: { tag: 'xsuaa' },
+    registry: { tag: 'SaaS' }
+});
+
+
+const xssec = require('@sap/xssec');
+const passport = require('passport');
+passport.use('JWT', new xssec.JWTStrategy(services.uaa));
+app.use(passport.initialize());
+app.use(passport.authenticate('JWT', {
+    session: false
+}));
+
+app.use(bodyParser.json());
+
+
+
+// subscribe/onboard a subscriber tenant
+app.put('/callback/v1.0/tenants/*', function (req, res) {
+    let tenantHost = req.body.subscribedSubdomain + '-' + appEnv.app.space_name.toLowerCase().replace(/_/g, '-') + '-' + services.registry.appName.toLowerCase().replace(/_/g, '-');
+    let tenantURL = 'https:\/\/' + tenantHost + /\.(.*)/gm.exec(appEnv.app.application_uris[0])[0];
+    console.log('Subscribe: ', req.body.subscribedSubdomain, req.body.subscribedTenantId, tenantHost, tenantURL);
+    res.status(200).send(tenantURL);
+});
+
+// unsubscribe/offboard a subscriber tenant
+app.delete('/callback/v1.0/tenants/*', function (req, res) {
+    let tenantHost = req.body.subscribedSubdomain + '-' + appEnv.app.space_name.toLowerCase().replace(/_/g, '-') + '-' + services.registry.appName.toLowerCase().replace(/_/g, '-');
+    console.log('Unsubscribe: ', req.body.subscribedSubdomain, req.body.subscribedTenantId, tenantHost);
+    res.status(200).send('');
+});
+
+
+// app user info
+app.get('/srv/info', function (req, res) {
+    if (req.authInfo.checkScope('$XSAPPNAME.User')) {
+        let info = {
+            'userInfo': req.user,
+            'subdomain': req.authInfo.getSubdomain(),
+            'tenantId': req.authInfo.getZoneId()
+        };
+        res.status(200).json(info);
+    } else {
+        res.status(403).send('Forbidden');
+    }
+});
+
+
+
+
+const port = process.env.PORT || 5001;
+app.listen(port, function () {
+    console.info('Listening on http://localhost:' + port);
+});
